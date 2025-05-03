37e589488d8fdda5fec9bc7223d0c22dcc2966bb
Ali Yahya Amer <aliyahyaamer@gmail.com>
2025-05-03 14:11:47 +0500

    Update

--- EDU_Track-master/components/student/sidebar.js
+++ EDU_Track-master/components/student/sidebar.js
@@ -13,22 +13,24 @@ import {
   IconBook,
   IconCertificate,
   IconBooks,
-  IconCoin
+  IconCoin,
+  IconBellRinging
 } from "@tabler/icons-react";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { cn } from "@/utils/cn";
-import Cookies from 'js-cookie'; // Import js-cookie to handle cookies
 import { useRouter } from 'next/navigation';
-import {removeData} from "@/utils/auth"; // Import useRouter
+import { removeData } from "@/utils/auth";
 
 export default function StudentSidebar() {
-
-
-  const router = useRouter(); // Initialize useRouter
+  const router = useRouter();
 
   const handleDashboard = () => {
     router.push('/studentPage/dashboard');
+  };
+
+  const handleAnnouncements = () => {
+    router.push('/studentPage/announcements');
   };
 
   const handleAttendance = () => {
@@ -73,15 +75,23 @@ export default function StudentSidebar() {
       label: "Dashboard",
       href: "#",
       icon: (
-          <IconBrandTabler className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconBrandTabler className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
       onClick: handleDashboard,
+    },
+    {
+      label: "Announcements",
+      href: "#",
+      icon: (
+        <IconBellRinging className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+      onClick: handleAnnouncements,
     },
     {
       label: "Attendance",
       href: "#",
       icon: (
-          <IconCheckupList className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconCheckupList className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
       onClick: handleAttendance,
     },
@@ -89,7 +99,7 @@ export default function StudentSidebar() {
       label: "Marks",
       href: "#",
       icon: (
-          <IconReportAnalytics className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconReportAnalytics className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
       onClick: handleMarks,
     },
@@ -97,7 +107,7 @@ export default function StudentSidebar() {
       label: "Time Table",
       href: "#",
       icon: (
-          <IconClock className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconClock className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
       onClick: handleTimetable,
     },
@@ -105,7 +115,7 @@ export default function StudentSidebar() {
       label: "Exam Schedule",
       href: "#",
       icon: (
-          <IconCalendar className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconCalendar className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
       onClick: handleExamschedule,
     },
@@ -113,7 +123,7 @@ export default function StudentSidebar() {
       label: "Assignment",
       href: "#",
       icon: (
-          <IconClipboardList className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconClipboardList className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
       onClick: handleAssignment,
     },
@@ -121,7 +131,7 @@ export default function StudentSidebar() {
       label: "Transcript",
       href: "#",
       icon: (
-          <IconCertificate className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconCertificate className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
       onClick: handleTranscript,
     },
@@ -129,7 +139,7 @@ export default function StudentSidebar() {
       label: "Books",
       href: "#",
       icon: (
-          <IconBooks className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconBooks className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
       onClick: handleBooks,
     },
@@ -137,7 +147,7 @@ export default function StudentSidebar() {
       label: "Fees",
       href: "#",
       icon: (
-          <IconCoin className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconCoin className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
       onClick: handleFees,
     },
@@ -145,7 +155,7 @@ export default function StudentSidebar() {
       label: "Logout",
       href: "#",
       icon: (
-          <IconArrowLeft className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+        <IconArrowLeft className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
       onClick: handleLogout,
     },
@@ -154,49 +164,51 @@ export default function StudentSidebar() {
   const [open, setOpen] = useState(false);
 
   return (
-      (<div
-          className={cn(
-              "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
-              "h-screen"
-          )}>
-        <Sidebar open={open} setOpen={setOpen}>
-          <SidebarBody className="justify-between gap-10">
-            <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-              {open ? <Logo /> : <LogoIcon />}
-              <div className="mt-8 flex flex-col gap-2">
-                {links.map((link, idx) => (
-                    <SidebarLink key={idx} link={link} />
-                ))}
-              </div>
+    <div
+      className={cn(
+        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
+        "h-screen"
+      )}>
+      <Sidebar open={open} setOpen={setOpen}>
+        <SidebarBody className="justify-between gap-10">
+          <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+            {open ? <Logo /> : <LogoIcon />}
+            <div className="mt-8 flex flex-col gap-2">
+              {links.map((link, idx) => (
+                <SidebarLink key={idx} link={link} />
+              ))}
             </div>
-          </SidebarBody>
-        </Sidebar>
-      </div>)
+          </div>
+        </SidebarBody>
+      </Sidebar>
+    </div>
   );
 }
+
 export const Logo = () => {
   return (
-      (<Link
-          href="#"
-          className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20">
-        <div
-            className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
-        <motion.span
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            className="font-medium text-black dark:text-white whitespace-pre">
-          Edu Track
-        </motion.span>
-      </Link>)
+    <Link
+      href="#"
+      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20">
+      <div
+        className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="font-medium text-black dark:text-white whitespace-pre">
+        Edu Track
+      </motion.span>
+    </Link>
   );
 };
+
 export const LogoIcon = () => {
   return (
-      (<Link
-          href="#"
-          className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20">
-        <div
-            className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
-      </Link>)
+    <Link
+      href="#"
+      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20">
+      <div
+        className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+    </Link>
   );
 };
